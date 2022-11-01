63a9a61394533802d6d716447bdfc84450ae8d02
chanjeff520 <chanjeff520@gmail.com>
2022-11-01 12:24:38 -0400

    add addEmployee function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 
 const mysql = require("mysql2");
+const { last } = require("rxjs");
 
 const db = mysql.createConnection(
     {
@@ -27,7 +28,7 @@ function viewAllRoles(){
     const sql = `SELECT role.title AS Job Title, role.id, department.name AS Department Name, role.salary AS Salary
                 FROM role
                 JOIN department ON role.department = department.id;`
-    db.query(sql, (err, rows) => {
+    db.query(sql, (err, res) => {
         if(err) throw err;
         console.table(res);
     })
@@ -38,12 +39,58 @@ function viewAllEmployees(){
     const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS Job Title, department.name As Department Name, role.salary, CONCAT(e.first_name, ' ', e.last_name) AS Manager
                 FROM employee
                 INNER JOIN role on role.id = employee.role_id
-                INNER JOIN `
-}
+                INNER JOIN department on department.id = role.department_id
+                LEFT JOIN employee e on employee.manager_id = e.id;`;
+    db.query(sql, (err,res) =>{
+        if(err) throw err;
+        console.table(res);
+    });
+};
 //add department
+function addDepartment(newDepartment){
+    const sql = 'INSERT INTO department(name) VALUES (?)';
+    db.query(sql, newDepartment, (err,res) => {
+        if (err){
+            console.log("New Department was not added.")
+            throw err;
+        }
+        console.log("New department have been added.");
+    })
+}
 
 //add a role
+function addRole(title, salary, departmentId){
+    const values = title + ", " + salary +", " + departmentId;
+    const sql = 'INSERT INTO role(title, salary, department) VALUES (?);';
+    db.query(sql, values, (err,res) => {
+        if (err){
+            console.log("New Department was not added.")
+            throw err;
+        }
+        console.log("New Role has been added.");
+    })
+}
 
 //add an empolyee
+function addEmployeee(firstName, LastName, roleId, departmentId){
+    const values = firstName + ", " + LastName + ", " + roleId + ", " + departmentId;
+    const sql = 'INSERT INTO employee(first_name , last_name, role, department) VALUES (?);';
+    db.query(sql, values, (err,res) => {
+        if (err){
+            console.log("New Department was not added.")
+            throw err;
+        }
+        console.log("New employee has been added.");
+    });
+}
 
-//update employee roll
+//update employee roll'
+
+function intro(){
+    console.log(`
+    
+    
+    
+    
+    `)
+}
