083b5f4a8d9d1453d5a1004a608d35cb045b837b
chanjeff520 <chanjeff520@gmail.com>
2022-11-01 20:36:17 -0400

    update views so that it runs correctly

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const { last } = require("rxjs");
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        port: 3001,
         user: 'root',
         password: 'password',
         database: 'employees_db'
@@ -56,46 +55,62 @@ function initalPrompt(){
 
 //view all departments
 function viewAllDepartments(){
-    const sql = `SELECT * FROM departments;`;
-    db.query(sql, (err, rows) => {
+    const sql = `SELECT * FROM department;`;
+    db.query(sql, (err, res) => {
         if(err) throw err;
         console.table(res);
+        initalPrompt();
     });
-    initalPrompt();
 }
 
 //view all roles
 function viewAllRoles(){
-    const sql = `SELECT role.title AS Job Title, role.id, department.name AS Department Name, role.salary AS Salary
+    const sql = `SELECT role.title, role.id, department.name AS department, role.salary
                 FROM role
-                JOIN department ON role.department = department.id;`
+                JOIN department ON role.department_id = department.id;`
     db.query(sql, (err, res) => {
         if(err) throw err;
         console.table(res);
+        initalPrompt();
     });
-    initalPrompt();
+
 }
 
 //view all emloyees
 function viewAllEmployees(){
-    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS Job Title, department.name As Department Name, role.salary, CONCAT(e.first_name, ' ', e.last_name) AS Manager
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS Job_Title, department.name As Department_Name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
                 FROM employee
+                LEFT JOIN role ON employee.role_id = role.id
+                LEFT JOIN department ON role.department_id = department.id
+                LEFT JOIN employee manager ON employee.manager_id = manager.id
                 ;`;
     db.query(sql, (err,res) =>{
         if(err) throw err;
         console.table(res);
+        initalPrompt();
     });
 };
 //add department
-function addDepartment(newDepartment){
-    const sql = 'INSERT INTO department(name) VALUES (?)';
-    db.query(sql, newDepartment, (err,res) => {
+function addDepartment(){
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Input a new Department",
+            name: "newDepartment",
+        }
+    ]).then( (res) => {
+        const sql = 'INSERT INTO department(name) VALUES (?)';
+        db.query(sql, res.newDepartment, (err,res) => {
         if (err){
             console.log("New Department was not added.")
             throw err;
         }
         console.log("New department have been added.");
+        initalPrompt();
+        });
     });
+
 }
 
 //add a role
@@ -113,6 +128,13 @@ function addRole(title, salary, departmentId){
 
 //add an empolyee
 function addEmployee(firstName, LastName, roleId, departmentId){
+
+    inquirer.prompt([
+        {
+            type: "",
+        }
+    ])
+
     const values = firstName + ", " + LastName + ", " + roleId + ", " + departmentId;
     const sql = 'INSERT INTO employee(first_name , last_name, role_id, department_id) VALUES (?);';
     db.query(sql, values, (err,res) => {
@@ -125,8 +147,33 @@ function addEmployee(firstName, LastName, roleId, departmentId){
 }
 
 //update employee role'
-function updateEmployee(roleId){
-    const sql = `SELECT  `
+function updateEmployee(){
+    const sql = 'SELECT * FROM employee';
+    
+    db.query(sql, (err,res) => {
+        console.log(res);
+        const employeeArr = res.map((employee) => {
+            return {
+                name: employee.first_name + " " +employee.last_name,
+                value: employee.id
+            }
+        });
+        console.log(employeeArr);
+    })
+
+    //list provide list of employee to user if hte user select one,
+    //we can get the employeeid
+    //make a list of the role and show the list to hte user,
+    //user can select a role, then we know about the employe id and role id
+    //update using employee id and role.id
+
+    // inquirer.prompt([
+    //     {
+    //         type: "list",
+
+    //     }
+    // ])
+
 }
 
 
@@ -139,3 +186,5 @@ function intro(){
     
     `)
 }
+
+updateEmployee();
